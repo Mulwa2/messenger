6bdab098daca5af303951836f7d9fe4fb2cb84e3
Mulwa2 <mwalai.mulwa1@students.jkuat.ac.ke>
2022-09-15 13:09:38 +0300

    login/regester page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:messenger/Screens/login.dart';
 import 'package:messenger/theme.dart';
 
 import 'Screens/HomeScreen.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: appTheme.light,
       darkTheme: appTheme.dark,
       themeMode: ThemeMode.light,
-      home: HomeScreen(),
+      home: const MyLogin(),
     );
   }
 }
